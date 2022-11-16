731997de80167bd59a440c8e6b98eeadf0be4dff
Justyna Boesche <justyna.boesche@gmail.com>
2022-11-16 17:12:57 +0100

    functions in js added

--- js/script.js
+++ js/script.js
@@ -1,50 +1,50 @@
-console.log("Cześć! Welcome GIT");
+{
+    const welcome = () => {
+        console.log("Cześć! Welcome GIT")
+    };
 
-let changeBackgroundButtonElement = document.querySelector(".js-changeBackgroundButton");
-let bodyElement = document.querySelector(".js-body");
-let changeColourElement = document.querySelector(".js-changeColour");
-let navigation__linkElements = document.querySelectorAll(".navigation__link")
-let imageOutlineElement = document.querySelector(".js-image");
-let sectionElements = document.querySelectorAll(".js-section")
-let articleBlueBackgroundElement = document.querySelector(".article--blueBackground")
-let article__foreignWordElements = document.querySelectorAll(".article__foreignWord")
-let section__linkElement = document.querySelector(".section__link")
+    const changeBackground = () => {
+        const bodyElement = document.querySelector(".js-body");
+        const changeColourElement = document.querySelector(".js-changeColour");
+        const navigation__linkElements = document.querySelectorAll(".navigation__link")
+        const imageOutlineElement = document.querySelector(".js-image");
+        const sectionElements = document.querySelectorAll(".js-section")
+        const articleBlueBackgroundElement = document.querySelector(".article--blueBackground")
+        const article__foreignWordElements = document.querySelectorAll(".article__foreignWord")
+        const section__linkElement = document.querySelector(".section__link")
 
-changeBackgroundButtonElement.addEventListener("click", () => {
-    bodyElement.classList.toggle("body--red");
+        bodyElement.classList.toggle("body--red");
+        navigation__linkElements.forEach((navigation__linkElement) => {
+            navigation__linkElement.classList.toggle("navigation__link--red");
+        });
+        imageOutlineElement.classList.toggle("main__image--red");
+        sectionElements.forEach((sectionElement) => {
+            sectionElement.classList.toggle("section--red");
+        });
+        articleBlueBackgroundElement.classList.toggle("article--redBackground");
+        article__foreignWordElements.forEach((article__foreignWordElement) => {
+            article__foreignWordElement.classList.toggle("article__foreignWord--red");
+        });
+        section__linkElement.classList.toggle("section__link--red");
+        changeColourElement.innerText = bodyElement.classList.contains("body--red") ? "niebieski" : "czerwony";
+    };
 
-    navigation__linkElements.forEach((navigation__linkElement) => {
-        navigation__linkElement.classList.toggle("navigation__link--red");
-    });
+    const showRemoveImage = () => {
+        const imageElement = document.querySelector(".js-image");
+        const showRemoveElement = document.querySelector(".js-showRemove");
+        imageElement.classList.toggle("main__imageRemove");
+        showRemoveElement.innerText = imageElement.classList.contains("main__imageRemove") ? "Pokaż" : "Ukryj";
+    };
 
-    imageOutlineElement.classList.toggle("main__image--red");
+    const init = () => {
+        const changeBackgroundButtonElement = document.querySelector(".js-changeBackgroundButton");
+        changeBackgroundButtonElement.addEventListener("click", changeBackground);
+        const showRemoveImageButtonElement = document.querySelector(".js-showRemoveImageButton");
+        showRemoveImageButtonElement.addEventListener("click", showRemoveImage);
 
-    sectionElements.forEach((sectionElement) => {
-        sectionElement.classList.toggle("section--red");
-    });
+        welcome();
+    };
 
-    articleBlueBackgroundElement.classList.toggle("article--redBackground");
-
-    article__foreignWordElements.forEach((article__foreignWordElement) => {
-        article__foreignWordElement.classList.toggle("article__foreignWord--red");
-    });
-
-    section__linkElement.classList.toggle("section__link--red");
-
-
-    changeColourElement.innerText = bodyElement.classList.contains("body--red") ? "niebieski" : "czerwony";
-});
-
-
-let showRemoveImageButtonElement = document.querySelector(".js-showRemoveImageButton");
-let imageElement = document.querySelector(".js-image");
-let showRemoveElement = document.querySelector(".js-showRemove");
-
-showRemoveImageButtonElement.addEventListener("click", () => {
-    imageElement.classList.toggle("main__imageRemove");
-
-    showRemoveElement.innerText = imageElement.classList.contains("main__imageRemove") ? "Pokaż" : "Ukryj"
-});
-
-
+    init();
+}
 
