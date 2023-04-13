46b6886fd55e46dda216d5e8bce8311a1c276ac1
Justyna Boesche <justyna.boesche@gmail.com>
2023-04-13 13:29:32 +0200

    color, background-color changes & implementation of changing color from blue to red for some more elements

--- js/script.js
+++ js/script.js
@@ -7,19 +7,27 @@
         const bodyElement = document.querySelector(".js-body");
         const changeColourElement = document.querySelector(".js-changeColour");
         const buttons = document.querySelectorAll(".js-button")
+        const headerElement = document.querySelector(".js-header__title")
         const navigation__linkElements = document.querySelectorAll(".navigation__link")
+        const article__headerElements = document.querySelectorAll(".article__header")
         const imageOutlineElement = document.querySelector(".js-image");
         const sectionElements = document.querySelectorAll(".js-section")
         const articleBlueBackgroundElement = document.querySelector(".article--blueBackground")
         const article__foreignWordElements = document.querySelectorAll(".article__foreignWord")
         const section__linkElement = document.querySelector(".section__link")
+        const tableHeaderElements = document.querySelectorAll(".table__cell--blueHeader")
+        const footerElement = document.querySelector(".js-footer")
 
         bodyElement.classList.toggle("body--red");
+        headerElement.classList.toggle("header__title--red");
         buttons.forEach((button) => {
             button.classList.toggle("button--red");
         });
         navigation__linkElements.forEach((navigation__linkElement) => {
             navigation__linkElement.classList.toggle("navigation__link--red");
+        });
+        article__headerElements.forEach((article__headerElement) => {
+            article__headerElement.classList.toggle("article__header--red");
         });
         imageOutlineElement.classList.toggle("main__image--red");
         sectionElements.forEach((sectionElement) => {
@@ -30,6 +38,10 @@
             article__foreignWordElement.classList.toggle("article__foreignWord--red");
         });
         section__linkElement.classList.toggle("section__link--red");
+        tableHeaderElements.forEach((tableHeaderElement) => {
+            tableHeaderElement.classList.toggle("table__cell--redHeader");
+        });
+        footerElement.classList.toggle("footer--red");
         changeColourElement.innerText = bodyElement.classList.contains("body--red") ? "niebieski" : "czerwony";
     };
 
